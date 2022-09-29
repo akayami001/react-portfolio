8b9f9c5a653a84413a16ba75cb12074c0ea058b0
mehmet <beyzadetoprak@gmail.com>
2022-09-29 10:51:14 +0200

    new change

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,9 +3,9 @@ import arrow from "../../images/arrow.png";
 import mobile from "../../images/mobile.png";
 import globe from "../../images/globe.png";
 import writing from "../../images/writing.png";
-import img1 from "../../images/design.jpg";
-import img2 from "../../images/react.jpg";
-import img3 from "../../images/fullstack.jpg";
+import des1 from "../../images/design.jpg";
+import des2 from "../../images/react.jpg";
+import des3 from "../../images/fullstack.jpg";
 
 const Works = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
@@ -15,21 +15,21 @@ const Works = () => {
       icon: `${mobile}`,
       title: "Web Design",
       desc: "Responsive Web Design",
-      img: img1,
+      img: des1,
     },
     {
       id: "2",
       icon: `${globe}`,
       title: "React Applications",
       desc: "React applications with REST API",
-      img: img2,
+      img: des2,
     },
     {
       id: "3",
       icon: `${writing}`,
       title: "MERN Stack Development",
       desc: "Developed with MERN stack Technologies",
-      img: img3,
+      img: des3,
     },
   ];
 
@@ -59,7 +59,7 @@ const Works = () => {
                 </div>
               </div>
               <div className="right">
-                <img src={d.img} alt="image" />
+                <img src={d.img} alt="" />
               </div>
             </div>
           </div>
